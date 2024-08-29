00142bbbb5852063ab0fcaa2f9c0ea442fc57b2c
jjco1996 <jjorozco@unimonserrate.edu.co>
2024-08-28 23:13:17 -0500

    Se crearon las migraciones en la base de datos

---
+++ database/migrations/2024_08_29_031901_create_transformations_detail_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transformations_detail', function (Blueprint $table) {
+            $table->id('transformation_detail_id');
+            $table->foreignId('transformation_id')->constrained('transformations', 'transformation_id')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('inventory_id')->constrained('inventories', 'inventory_id')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transformations_detail');
+    }
+};
