537feccc95638c629701058d192cdbaae77825f3
edrianlopez <edrianlopez95@gmail.com>
2024-08-29 21:19:17 -0500

    Se crea la llave foranea de inventarios en la tabla detalle de solicitudes

--- database/migrations/2024_08_29_051026_create_requests_detail_table.php
+++ database/migrations/2024_08_29_051026_create_requests_detail_table.php
@@ -10,9 +10,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('requests_detail', function (Blueprint $table) {
-            $table->id('surequests_detail_id');
+            $table->id('request_detail_id');
             $table->foreignId('request_id')->constrained('requests', 'request_id')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->integer('inventory_id')->constrained('inventories', 'inventory_id')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('inventory_id')->constrained('inventories', 'inventory_id')->cascadeOnUpdate()->cascadeOnDelete();
             $table->integer('quantity');
             $table->timestamps();
         
