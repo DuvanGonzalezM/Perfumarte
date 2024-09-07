5d169abb78b3e6914fd985413d9fcc1ff89875c6
edrianlopez <edrianlopez95@gmail.com>
2024-09-07 00:43:19 -0500

    se actualiza el layout de invitado

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,27 +1,14 @@
 <script setup>
-import CardButton from '@/Components/CardButton.vue';
-import InformationCard from '@/Components/InformationCard.vue';
-import ButtonSidebar from '@/Components/ButtonSidebar.vue';
-import Sidebar from '@/Components/Sidebar.vue';
 import Image from '@/Components/Image.vue';
-import { Link } from '@inertiajs/vue3';
 </script>
-
 <template>
     <div class="card card-login align-items-center">
         <div class="over-card position-relative align-items-center d-flex flex-column">
-            <Image  src="assets\images\Logo_3.png" />
+            <Image src="assets\images\Logo_3.png" />
             <h3>Inicio de Sesión</h3>
         </div>
         <div class="card-body position-relative">
             <slot />
-
         </div>
-        <Link href="/">
-        <InformationCard />
-        <CardButton />
-        <ButtonSidebar />
-        <Sidebar />
-        </Link>
     </div>
 </template>
