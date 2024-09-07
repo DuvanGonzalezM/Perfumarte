d41213a7f47e270d804c6cec83ff6c116cb22bd9
Duvan Gonzalez <dgonzalez@aplyca.com>
2024-09-07 03:21:47 -0500

    Se ajusta el layout base de todas las paginas

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,14 +1,17 @@
 <script setup>
 import Image from '@/Components/Image.vue';
 </script>
+
 <template>
-    <div class="card card-login align-items-center">
-        <div class="over-card position-relative align-items-center d-flex flex-column">
-            <Image src="assets\images\Logo_3.png" />
-            <h3>Inicio de Sesión</h3>
-        </div>
-        <div class="card-body position-relative">
-            <slot />
+    <div class="container">
+        <div class="card card-login align-items-center">
+            <div class="over-card position-relative align-items-center d-flex flex-column">
+                <Image src="assets\images\Logo_3.png" />
+                <h3>Inicio de Sesión</h3>
+            </div>
+            <div class="card-body position-relative">
+                <slot />
+            </div>
         </div>
     </div>
 </template>
