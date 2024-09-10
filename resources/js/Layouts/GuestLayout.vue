d59293e07619ce9df3cbfce125f5982157cd202a
jjco1996 <jjorozco@unimonserrate.edu.co>
2024-09-09 20:47:28 -0500

    Revision GIT componentes

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,18 +4,9 @@ import Alert from '@/Components/Alert.vue';
 
 import Image from '@/Components/Image.vue';
 </script>
-<<<<<<< HEAD
-
-
-
-<template>
-    <div class="card card-login">
-        <div class="login-logo">
-=======
 <template>
     <div class="card card-login align-items-center">
         <div class="over-card position-relative align-items-center d-flex flex-column">
->>>>>>> 842d2e2755172ca9d826ef2e6cf3f0d17538d224
             <Image src="assets\images\Logo_3.png" />
             <h3>Inicio de Sesión</h3>
         </div>
