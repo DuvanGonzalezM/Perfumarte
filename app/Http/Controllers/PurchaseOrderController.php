dc8c51bb96a3c0d98488bd5bff9aa9fc4e8bca7c
Jorge Orozco <jjorozco@unimonserrate.edu.co>
2024-09-19 22:54:27 -0500

    Se realiza componente select

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -18,21 +18,4 @@ class PurchaseOrderController extends Controller
 
         dd($supplier, $products);
     }
-
-    public function edit(PurchaseOrder $purchase_order): View
-    {
-        return view('EditPurchaseOrder')->with('purchase_order', $purchase_order);
-    }
-
-    public function update(Request $request, $purchase_order_id)
-    {
-        $request->validate([
-            'supplier_order' => 'required|string|max:255',
-        ]);
-        $purchase_order = PurchaseOrder::findOrFail($purchase_order_id);
-        $purchase_order->supplier_order = $request->supplier_order;
-        $purchase_order->save();
-        return redirect()->route('purchase_orders.edit', $purchase_order->$purchase_order_id)
-            ->with('success', 'Orden de compra actualizada exitosamente.');
-    }
 }
