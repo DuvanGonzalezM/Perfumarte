490883cd0f84bb662790e58b70618dc0e65b1a8e
Duvan Gonzalez <dgonzalez@aplyca.com>
2025-01-31 20:12:00 -0500

    Se agrega validacion para el inventario inicial

--- app/Http/Controllers/InventoryLocationController.php
+++ app/Http/Controllers/InventoryLocationController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CashRegister;
 use App\Models\InventoryValidation;
 use App\Models\Inventory;
-use App\Models\Location;
-use App\Models\Warehouse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -24,11 +23,17 @@ class InventoryLocationController extends Controller
         if ($hasAcceptedToday) {
             return redirect()->route('inventory.current');
         }
+        $userLocation = $user->with('location_user.warehouses')->where('user_id', $user->user_id)->first();
+        $location_user = $userLocation->location_user;
+        $warehouses = $location_user[0]->warehouses;
+        $inventory = null;
+        if(count($warehouses) > 0){
+            $inventory = Inventory::with(relations: 'product')->where('warehouse_id', $warehouses[0]->warehouse_id)->get();
+        }
 
-        $warehouses = $user->location->with('warehouses')->where('location_id', $user->location_id)->first();
-        $inventory = Inventory::with('product')->where('warehouse_id', $warehouses->warehouses[0]->warehouse_id)->get();
         return Inertia::render('Stock/StartInventoryLocation', [
             'initialInventory' => $inventory,
+            'location' => $location_user[0],
             'sidebarHidden' => true
         ]);
     }
@@ -41,6 +46,19 @@ class InventoryLocationController extends Controller
             'location_id' => $user->location_id,
             'date' => Carbon::today(),
             'accepted_at' => Carbon::now(),
+        ]);
+
+        CashRegister::create([
+            'location_id' => $user->location_id,
+            'total_collected' => 0,
+            'count_100_bill' => 0,
+            'count_50_bill' => $request->count_50_bill ?? 0,
+            'count_20_bill' => $request->count_20_bill ?? 0,
+            'count_10_bill' => $request->count_10_bill ?? 0,
+            'count_5_bill' => $request->count_5_bill ?? 0,
+            'count_2_bill' => $request->count_2_bill ?? 0,
+            'count_1_bill' => $request->count_1_bill ?? 0,
+            'total_coins' => $request->total_coins ?? 0,
         ]);
 
         return redirect()->route('inventory.current');
--- resources/js/Pages/Stock/StartInventoryLocation.vue
+++ resources/js/Pages/Stock/StartInventoryLocation.vue
@@ -7,9 +7,12 @@ import Table from '@/Components/Table.vue';
 import BaseLayout from '@/Layouts/BaseLayout.vue';
 import ModalPrais from '@/Components/ModalPrais.vue';
 import { Head } from '@inertiajs/vue3';
+import TextInput from '@/Components/TextInput.vue';
+import SelectSearch from '@/Components/SelectSearch.vue';
 
 const props = defineProps({
     initialInventory: Object,
+    location: Object,
     sidebarHidden: Boolean
 });
 
@@ -33,6 +36,13 @@ const columnsTable = [
 
 const form = useForm({
     accepted: false,
+    count_50_bill: null,
+    count_20_bill: null,
+    count_10_bill: null,
+    count_5_bill: null,
+    count_2_bill: null,
+    count_1_bill: null,
+    total_coins: null,
     inventoryData: props.initialInventory,
 });
 
@@ -47,6 +57,7 @@ const closeModal = () => {
 };
 
 const handleSubmit = () => {
+    form.accepted = true;
     form.post(route('inventory.accept'), {
         onSuccess: () => {
             window.location.href = route('inventory.current');
@@ -59,7 +70,7 @@ const handleSubmit = () => {
 <template>
     <Head title="Inventario Inicial" />
     <BaseLayout :loading="form.processing ? true : false">
-        <SectionCard :subtitle="'Base: $500.000'">
+        <SectionCard :subtitle="'Base: $'+props.location.cash_base">
             <template #headerSection>
                 <strong>Inventario Inicial</strong>
             </template>
@@ -74,14 +85,58 @@ const handleSubmit = () => {
 
         <ModalPrais v-model="showModal" @close="closeModal">
             <template #header>
-                <h3>Confirmación</h3>
+                <h4>¿Confirma la base y el inventario inicial para continuar con el módulo?</h4>
             </template>
             <template #body>
-                <p>¿Confirma la base y el inventario inicial para iniciar el módulo?</p>
+                <div class="row">
+                    <h5>Cantidades en caja ${{ (form.count_50_bill * 50000) + (form.count_20_bill * 20000) + (form.count_10_bill * 10000) + (form.count_5_bill * 5000) + (form.count_2_bill * 2000) + (form.count_1_bill * 1000) + (form.total_coins * 1) }}</h5>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_50_bill" id="count_50_bill" v-model="form.count_50_bill"
+                            :focus="form.count_50_bill != null ? true : false" labelValue="Cantidad de billetes de 50 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_20_bill" id="count_20_bill" v-model="form.count_20_bill"
+                            :focus="form.count_20_bill != null ? true : false" labelValue="Cantidad de billetes de 20 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_10_bill" id="count_10_bill" v-model="form.count_10_bill"
+                            :focus="form.count_10_bill != null ? true : false" labelValue="Cantidad de billetes de 10 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_5_bill" id="count_5_bill" v-model="form.count_5_bill"
+                            :focus="form.count_5_bill != null ? true : false" labelValue="Cantidad de billetes de 5 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_2_bill" id="count_2_bill" v-model="form.count_2_bill"
+                            :focus="form.count_2_bill != null ? true : false" labelValue="Cantidad de billetes de 2 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <TextInput type="number" name="count_1_bill" id="count_1_bill" v-model="form.count_1_bill"
+                            :focus="form.count_1_bill != null ? true : false" labelValue="Cantidad de billetes de 1 mil"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                    <div class="col-md-12 mt-4">
+                        <TextInput type="number" name="total_coins" id="total_coins" v-model="form.total_coins"
+                            :focus="form.total_coins != null ? true : false" labelValue="Cantidad de monedas"
+                            :minimo="0"
+                            :required="true" />
+                    </div>
+                </div>
             </template>
             <template #footer>
                 <PrimaryButton @click="closeModal" class="mx-5 px-5">No</PrimaryButton>
-                <PrimaryButton @click="handleSubmit" class="mx-5 px-5">Sí</PrimaryButton>
+                <PrimaryButton @click="handleSubmit" :class="((form.count_50_bill * 50000) + (form.count_20_bill * 20000) + (form.count_10_bill * 10000) + (form.count_5_bill * 5000) + (form.count_2_bill * 2000) + (form.count_1_bill * 1000) + (form.total_coins * 1)) != (props.location.cash_base) ? 'disabled' : ''" class="mx-5 px-5">Sí</PrimaryButton>
             </template>
         </ModalPrais>
     </BaseLayout>
