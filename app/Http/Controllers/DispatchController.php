dbee3d93b03121ae94e735c6822e3c22caccfc9e
edrianlopez <edrianlopez95@gmail.com>
2025-07-04 22:42:50 -0500

    se realizan los cambios para la funcion de aprobar devolucion

--- app/Http/Controllers/DispatchController.php
+++ app/Http/Controllers/DispatchController.php
@@ -178,4 +178,63 @@ class DispatchController extends Controller
                 ->withErrors(['error' => 'Error al crear el despacho: ' . $e->getMessage()]);
         }
     }
+
+    public function getReturnedDispatch($id)
+    {
+        $user = auth()->user();
+
+        $dispatch = Dispatch::with([
+            'dispatchdetail.inventory.product',
+            'dispatchdetail.warehouse.location'
+        ])->findOrFail($id);
+
+        if ($dispatch->status == 'Recibido' && $user->hasRole('Monitoreo')) {
+            return Inertia::render('Dispatch/DispatchDetail', [
+                'dispatch' => $dispatch
+            ]);
+        }
+
+        return Inertia::render('Dispatch/Dispatchdetail', [
+            'dispatch' => $dispatch
+        ]);
+    }
+
+    public function storeReturnedQuantities(Request $request)
+    {
+        $request->validate([
+            'dispatch_id' => 'required|exists:dispatches,dispatch_id',
+            'details' => 'required|array',
+            'details.*.id' => 'required|exists:dispatches_detail,dispatchs_detail_id',
+            'details.*.returned_quantity' => 'required|numeric',
+        ]);
+
+        foreach ($request->details as $detailData) {
+            $detail = DispatchDetail::with('inventory')->find($detailData['id']);
+
+            $detail->returned_quantity = $detailData['returned_quantity'];
+            $detail->save();
+
+            $inventory = Inventory::where('product_id', $detail->inventory->product_id)
+                ->where('warehouse_id', 2)
+                ->first();
+
+            if ($inventory) {
+                $inventory->quantity += $detailData['returned_quantity'];
+                $inventory->save();
+            } else {
+                Inventory::create([
+                    'product_id' => $detail->inventory->product_id,
+                    'warehouse_id' => 2,
+                    'quantity' => $detailData['returned_quantity'],
+                ]);
+            }
+        }
+
+        $dispatch = Dispatch::findOrFail($request->dispatch_id);
+        $dispatch->status = 'Devuelto';
+        $dispatch->save();
+
+
+        return redirect()->route('dispatch.list')->with('success', 'Cantidades devueltas registradas en inventario.');
+    }
 }
